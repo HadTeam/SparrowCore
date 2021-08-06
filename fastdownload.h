5d60222593772169fbb6cd64d83ce496b08a80fc
玉米 <cw@had.ink>
2021-08-06 22:28:52 +0800

    [Feat](fastDownload): Declaration the class | 声明类

--- fastdownload.h
+++ fastdownload.h
@@ -1,11 +1,31 @@
 #ifndef FASTDOWNLOAD_H
 #define FASTDOWNLOAD_H
 
+#include <QtAlgorithms>
+#include <QQueue>
+#include <QString>
+
+typedef qint16 fastDownloadStatus;
+typedef qint8 fastDownloadQueueId;
+typedef quint32 filePoint;
+
+typedef struct fastDonwloadInfo {
+    QString savePath, remotePath;
+    fastDownloadStatus status;
+    fastDonwloadInfo() {
+        this->status=0;
+    }
+} fastDonwloadInfo;
 
 class fastDownload
 {
 public:
     fastDownload();
+    ~fastDownload();
+    void downloadLoop();
+    fastDownloadStatus startDownload(fastDownloadQueueId Id, filePoint startPos, filePoint endPos);
+
+    QQueue<fastDonwloadInfo> downloadQueue;
 };
 
 #endif // FASTDOWNLOAD_H
