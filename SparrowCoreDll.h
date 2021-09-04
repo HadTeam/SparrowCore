7b6bc6c5964a4924e7dd258aa321e4de7a0263b3
XiaoLi8848 <1662423349@qq.com>
2021-09-04 21:29:26 +0800

    refactor: Add global header

---
+++ SparrowCoreDll.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef SPARROW_CORE_DLL
+#define SPARROW_CORE_DLL
+#endif
+
+#pragma comment(lib,"SparrowCore.lib")
+#include "CommandLine.h"
+#include "fastDownload.h"
+#include "jsonParser.h"
+#include "Launcher.h"
+#include "Minecraft.h"
+#include "utils.h"
--- SparrowCore_global.h
+++ SparrowCore_global.h
@@ -2,11 +2,18 @@
 #define SPARROWCORE_GLOBAL_H
 
 #include <QtCore/qglobal.h>
+#include "utils.h"
 
-#if defined(SPARROWCORE_LIBRARY)
+#ifdef SPARROWCORE_LIBRARY
 #  define SPARROWCORE_EXPORT Q_DECL_EXPORT
 #else
 #  define SPARROWCORE_EXPORT Q_DECL_IMPORT
 #endif
+
+namespace test {
+	SPARROWCORE_EXPORT std::string inline getSystemVersion(void) {
+		return Sparrow::utils::getSystemVersion();
+	}
+}
 
 #endif // SPARROWCORE_GLOBAL_H
